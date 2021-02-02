200d6f2422d414800cf79800f785a8b7e3bb1b66
asriidev-gh <asriidev@gmail.com>
2021-02-02 20:27:05 +0800

    updates

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -2,12 +2,9 @@ import React, {useState, useEffect} from 'react';
 import { connect,useSelector,useDispatch } from "react-redux";
 import PersonIcon from '@material-ui/icons/Person';
 import { Link } from "react-router-dom";
-import { Person } from '@material-ui/icons';
 
-const Profile = ({user}) => {
-    
-    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-    const user = useSelector(state => state.auth.user);
+const Profile = () => {        
+    const user = useSelector(state => state.auth.user);    
 
     return (
         <div className="menu-item">
@@ -17,11 +14,4 @@ const Profile = ({user}) => {
     )
 }
 
-// const mapStateToProps = state => ({
-//     isAuthenticated: state.auth.isAuthenticated,
-//     user: state.auth.user,
-//     error: state.error
-// });
-
-// export default connect(mapStateToProps, null)(Profile);
 export default Profile;
